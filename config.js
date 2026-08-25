a2a9a78fcf430e75324dd658e1bf53c8e52e0d7b
pischayas <pischayas@gmail.com>
2026-08-25 20:13:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // วางลิงก์ Web App URL ที่ได้จากการ Deploy Google Apps Script ตรงนี้
 // ตัวอย่าง: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxxxxxxxxxxxxxx/exec
-const APPS_SCRIPT_URL = "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwhCz47pswFFH5CuUqUA0AVoviaExzhC7paT0caOWJ_InTm98yyZcXE72MGMxtiSalz/exec";
