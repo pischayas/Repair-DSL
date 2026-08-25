18da3ff28ec23beb8ed7426d8c463ab6825f6954
pischayas <pischayas@gmail.com>
2026-08-25 20:08:37 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// วางลิงก์ Web App URL ที่ได้จากการ Deploy Google Apps Script ตรงนี้
+// ตัวอย่าง: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxxxxxxxxxxxxxx/exec
+const APPS_SCRIPT_URL = "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
