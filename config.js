06ea4c999d6a03523653aea5ea2e3a7e12a4e2dd
pischayas <pischayas@gmail.com>
2026-09-14 00:12:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
 // วางลิงก์ Web App URL ที่ได้จากการ Deploy Google Apps Script ตรงนี้
 // ตัวอย่าง: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxxxxxxxxxxxxxx/exec
 const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwPjKUckEDBirIMzHmDRcUI7TSvLXasdblU6w2D4HboGgXnVWQOgms8ZzbfG0gLPszh/exec";
+const GOOGLE_CLIENT_ID = "868838247565-4nu28s7lloif7c6cenpg50l46ifjqs9i.apps.googleusercontent.com";
+// โดเมนอีเมลที่อนุญาตให้แก้ไขสถานะงานได้ (ไม่ต้องใส่ @ นำหน้า)
+const ALLOWED_EMAIL_DOMAIN = "dsl.ac.th";
